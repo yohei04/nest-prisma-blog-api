338282f1f1a27b24b658914d8f6a14a9775c3318
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-12-07 01:50:45 +0900

    feat: Dtoにバリデーションを追加

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty()
   name: string;
 
+  @IsEmail()
+  @IsNotEmpty()
   @ApiProperty()
   email: string;
 }
