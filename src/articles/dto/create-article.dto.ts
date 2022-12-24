614ecceed7a4cc7e192495a64542355921828bdd
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-12-24 23:23:37 +0900

    feat: CreateArticleDtoにバリデーションを追加

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,15 +1,25 @@
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateArticleDto {
-  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(20)
+  @ApiProperty({ maxLength: 20 })
   title: string;
 
+  @IsString()
+  @IsOptional()
   @ApiProperty({ required: false })
   description?: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   body: string;
 
+  @IsOptional()
   @ApiProperty({ required: false, default: false })
   published?: boolean = false;
 }
